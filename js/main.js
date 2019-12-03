03cf8f42cb18aae9c14a4f5e1cb0695ee568a9ca
Roman Zubko <skcorpoma@gmail.com>
2019-12-03 16:20:10 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function() {
+  $(".header__slider").slick({
+    infinite: true,
+    fade: true,
+    speed: 500,
+    cssEase: "linear",
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="" />',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="" />',
+    asNavFor: ".slider-dots"
+  });
+
+  $(".slider-dots").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: ".header__slider"
+  });
+});
